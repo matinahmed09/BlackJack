2c40932dd36993af92e9ad82fb27ee763b836331
flexninja21 <www.edem@gmail.com>
2021-11-18 10:58:37 +0000

    refactored class name for CardType Matin stated thats more in the current domain

---
+++ src/com/company/Suit.java
@@ -0,0 +1,8 @@
+package com.company;
+//In playing cards, a suit is one of the categories into which the cards of a deck are divided.
+public enum Suit {
+    Hearts,
+    Clubs,
+    Spades,
+    Diamonds
+}
