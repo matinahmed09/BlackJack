696d92205e9bc68b78ead12f652b4e4a80d73b27
MatinAhmed <abdul-matin.ahmed@turntabl.io>
2021-11-18 17:05:10 +0000

    Players update2

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,10 +1,12 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Player {
     private String name;
+    private List<Card> inhandCard = new ArrayList<>();
 
     public Player(String name) {
        this.name =name;
@@ -19,10 +21,10 @@ public class Player {
         this.name = name;
     }
 
-    /*
+
     public int getCardinHandValue(){
         int cardvalueinhand = 0;
-        for (Card card: getCardinHandValue()){
+        for (Card card: getinhandCard()){
             cardvalueinhand+= card.getNumber();
         }
         return cardvalueinhand;
@@ -32,7 +34,11 @@ public class Player {
 
     }
 
-    public List<Card> get
+    public List<Card> getinhandCard(){
+        return inhandCard;
 
-     */
+
+    }
+
+
 }
