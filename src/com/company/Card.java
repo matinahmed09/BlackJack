6858c4810dfe07a9c554e037331467dedf9ec966
flexninja21 <www.edem@gmail.com>
2021-11-18 11:08:21 +0000

    implemented set/get/string and vars for Card class

---
+++ src/com/company/Card.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Card {
+    private Suit suit;
+    private int number;
+
+    public Card(Suit suit, int number) {
+        this.suit = suit;
+        this.number = number;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "suit=" + suit +
+                ", number=" + number +
+                '}';
+    }
+}
