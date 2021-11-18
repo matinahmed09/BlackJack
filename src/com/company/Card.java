0d64b1f01303f9fb62a9f4573e25aea444918d84
flexninja21 <www.edem@gmail.com>
2021-11-18 11:18:19 +0000

    maitn

--- src/com/company/Card.java
+++ src/com/company/Card.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.List;
+
 public class Card {
     private Suit suit;
     private int number;
--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -1,4 +1,8 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Deck {
+   private Card card;
 }
